045b79654498f78d4a160e1c5c02aecca7062a99
Leo <leonelsicaro@gmail.com>
2025-06-04 19:46:22 +0200

    adding explore & gallery

--- assets/script.js
+++ assets/script.js
@@ -9,3 +9,42 @@ menuOpenButon.addEventListener("click",()=>{
 //Close mobile menu
 menuCloseButon.addEventListener("click",()=> menuOpenButon.click())
 
+
+
+const swiper = new Swiper('.slide-wrapper', {
+
+  loop: true,
+  spaceBetween:25,
+  grabCursor:true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable:true,
+    dynamicBullets:true,
+    
+
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  //responsive breakpoint
+
+  breakpoints:{
+    0:{
+        slidesPerView:1
+    },
+    768:{
+        slidesPerView:2
+    },
+    1024:{
+        slidesPerView:3
+    }
+  }
+
+
+});
